0bbe31e49aada1dea9364d2668963696ca8aaf48
zidaansm <zidanseftino18@gmail.com>
2026-07-04 20:34:52 +0700

    fix: delete unused test script to resolve build error

--- test-tiktok.ts
+++
@@ -1,8 +0,0 @@
-import { getTikTokVideoId } from "./src/lib/tiktok";
-
-async function test() {
-  const url = "https://www.tiktok.com/@ldvarch/video/7655947701607075093?is_from_webapp=1&sender_device=pc&web_id=7508842332423964167";
-  console.log("Extracted ID:", await getTikTokVideoId(url));
-}
-
-test();
